e090e85844fc2d43a27c5e2accc1d36c98630256
Octopopo <godpopo34@gmail.com>
2022-03-30 21:33:26 -0700

    1567. Maximum Length of Subarray With Positive Product
    This is a relative simple question, the solution can be cutting vector into multiple parts, and check each of them once, since we have two side, we can to a check from left to right.
    And do a reverse, and check again.

---
+++ 1567. Maximum Length of Subarray With Positive Product/Maximum_Length_of_Subarray_With_Positive_Product.cpp
@@ -0,0 +1,87 @@
+class Solution {
+public:
+    int getMaxLen(vector<int>& nums) {
+        int nL = nums.size();
+        vector<int> zeroPos;
+        vector<int> fromLeft(nL, 0);
+        int temp;
+        int maxLen = 0;
+        
+        fromLeft[0] = isPosOrNeg(nums[0]);
+        
+        if(nums[0] == 0){
+            zeroPos.push_back(0);
+        }
+        
+        for(int i = 1; i < nL; i++){
+            temp = isPosOrNeg(nums[i]);
+            if(temp == 0){
+                fromLeft[i] = 0;
+                zeroPos.push_back(i);
+            }
+            else{
+                if(fromLeft[i-1] == 0){
+                    fromLeft[i] = temp;
+                    continue;
+                }
+                else{
+                    fromLeft[i] = fromLeft[i-1] * temp;
+                }
+            }
+        }
+        int lastZ = -1;
+        int currentZ;
+        
+        for(int i = 0; i < zeroPos.size(); i++){
+            currentZ = zeroPos[i];
+            if(currentZ -  lastZ <= 1){
+                lastZ = currentZ;
+                continue;
+            }
+            else{
+                if(fromLeft[currentZ-1] > 0){
+                    maxLen = max((currentZ-lastZ-1), maxLen);
+                }
+                else{
+                    maxLen = max(maxLen,maxLenFromPart(nums, lastZ+1, currentZ-1));
+                }
+            }
+            lastZ = currentZ;
+        }
+        
+        if(fromLeft[nums.size()-1] > 0){
+            maxLen = max(((int)nums.size()-lastZ-1), maxLen);
+        }
+        else{
+            maxLen = max(maxLen,maxLenFromPart(nums, lastZ+1, nums.size()-1));
+        }
+        
+        return maxLen;
+    }
+    
+    int maxLenFromPart(vector<int>& nums, int start, int end){
+        if(start == end){
+            return nums[start] > 0 ? 1 : 0;
+        }
+        int nFromLeft = start, nFromRight = end;
+        while(nFromRight >= 0 && nums[nFromRight] > 0){
+            nFromRight--;
+        }
+        while(nFromLeft < nums.size() && nums[nFromLeft] > 0){
+            nFromLeft++;
+        }
+        
+        
+        return  max((nFromRight-start), (end-nFromLeft));
+    }
+    
+    int isPosOrNeg(int num){
+        if(num > 0){
+            return 1;
+        }
+        else if(num < 0){
+            return  -1;
+        }
+        return 0;
+    }
+};
